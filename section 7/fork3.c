4ff42f3203047a7bf2a467d8fb7f706ef8cca469
zhuxinquan <1174272148@qq.com>
2015-07-25 11:25:42 +0800

    孤儿进程

---
+++ section 7/fork3.c
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: fork3.c
+	> Author: zhuxinquan
+	> Mail: zhuxinquan61@gmail.com
+	> Created Time: 2015年07月25日 星期六 10时46分03秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    pid_t pid;
+
+    pid = fork();
+    switch(pid)
+    {
+        case 0:
+        {
+            while(1)
+            {
+                printf("child process is run, pid is %d, parent id is %d.\n", getpid(), getppid());
+                sleep(2);
+            }
+            break;
+        }
+        case -1:
+        {
+            perror("process creat failed:");
+            exit(-1);
+            break;
+        }
+        default:
+        {
+            printf("parent process is run!");
+            exit(0);
+        }
+    }
+    return 0;
+}
