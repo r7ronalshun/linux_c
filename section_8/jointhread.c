7dd5f2d658c71b6ad2e51c67cb7374349a38f29b
zhuxinquan <1174272148@qq.com>
2015-07-31 11:09:05 +0800

    pthread_join 函数等待辅助线程结束

---
+++ section_8/jointhread.c
@@ -0,0 +1,30 @@
+/*************************************************************************
+	> File Name: jointhread.c
+	> Author: zhuxinquan
+	> Mail: zhuxinquan61@gmail.com
+	> Created Time: 2015年07月31日 星期五 10时52分07秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+void assisthread(void * arg)
+{
+    printf("I'm help to do something!\n");
+    sleep(3);
+    pthread_exit(0);
+}
+
+int main(void)
+{
+    pthread_t assistthid;
+    int status;
+
+    pthread_create(&assistthid, NULL, (void *) assisthread, NULL);
+    pthread_join(assistthid, (void *)&status);
+    printf("assistthid's exit is caused %d\n", status);
+
+    return 0;
+}
