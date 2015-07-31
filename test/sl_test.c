72945fd11bf839b142148169c2ce74b30ad46a58
zhuxinquan <1174272148@qq.com>
2015-07-31 08:44:05 +0800

    跑火车

---
+++ test/sl_test.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: ./test/sl_test.c
+	> Author: zhuxinquan
+	> Mail: zhuxinquan61@gmail.com
+	> Created Time: 2015年07月31日 星期五 08时41分25秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    system("sl");
+}
