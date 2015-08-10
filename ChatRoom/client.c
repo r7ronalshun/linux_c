38154e8dfe7e20a8177460ec7d4edfa12ff2ae07
zhuxinquan <1174272148@qq.com>
2015-08-10 15:55:56 +0800

    add

--- ChatRoom/client.c
+++ ChatRoom/client.c
@@ -5,21 +5,75 @@
 	> Created Time: 2015年08月08日 星期六 17时15分06秒
  ************************************************************************/
 
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<unistd.h>
-#include<string.h>
-#include<errno.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<pthread.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include "inputkey_getch.h"
 
+#define BUFSIZE 1024 
+
+void cls(void);                         //清除输入缓存函数
 void my_err(const char *, int);         //错误处理函数
 void print_menu(void);                  //打印主界面菜单函数
 void quit(void);                        //退出函数
+int register_num();                    //注册函数
 
+
+struct info                             //个人信息
+{
+    char    username[10];
+};
+
+struct group                            //群组信息
+{
+    char            group_name[20];
+    char            admin_name[10];
+    struct info     member[10];
+};
+
+struct users                            //用户链表结构体
+{
+    char            password[10];
+    struct info     user;
+    struct info     friends[10];        //好友
+    struct group    group[5];
+    struct users*   next;
+};
+
+/*在客户端只有一个用户在使用
+ *所以可以将所有的用户的好友
+ *数、群组数等设置为全局变量
+ */
+int                 conn_fd;
+int                 friend_num;
+int                 group_num;
+int                 member_num;
+char                password[10];
+struct info         user;
+struct info         friend[10];
+struct info         member[10];
+struct info         group[3];
+
+
+struct chat                             //聊天消息结构体
+{
+    int     flag;
+    char    from[10];
+    char    to[10];
+    char    ask[10];
+    char    time[30];
+    char    news[500];
+};
+
+int             message_num  = 0;               //消息数量
+struct chat     message[100];                   //消息队列
 
 
 void my_err(const char * err_string, int line)
@@ -28,11 +82,19 @@ void my_err(const char * err_string, int line)
     perror(err_string);
     exit(1);
 }
+
+void cls()
+{
+    char c;
+    do{
+        c = fgetc(stdin);
+    }while(c != 10 && c != EOF);
+}
+
 int main(int argc, char ** argv)
 {
     int                  i;
     int                  ret;
-    int                  conn_fd;
     int                  serv_port;
     struct sockaddr_in   serv_addr;
 
@@ -105,11 +167,14 @@ void print_menu(void)
         printf("\t\t            2.登陆\n\n");
         printf("\t\t            3.退出\n\n");
         printf("\t\t----------------------------\n\n");
-        printf("\t\t        请选择<1 ~ 3>：\n");
+        printf("\t\t         请选择<1 ~ 3>：");
         c= getchar();
         if(c == 1)
         {
-            //注册函数
+            if(register_num())                  //注册返回值1,注册成功
+            {
+                break;
+            }
         }
         else if(c == 2)
         {
@@ -126,7 +191,73 @@ void print_menu(void)
 }
 
 
-void quit(void)
+void quit()
 {
-    
+    char quitbuf[1024];                         //接受退出时服务器的返回结果
+    send(conn_fd, "q", 1024, 0);
+    recv(conn_fd, quitbuf, 1024, 0);
+    if(strcmp(quitbuf, "y") == 0)
+    {
+        close(conn_fd);
+        system("clear");
+        exit(0);
+    }
+    else{
+        printf("quit error!\n");
+    }
+}
+
+int register_num()
+{
+    int         i;
+    char        register_buf[1024] = "0";
+    struct info user_temp;                      //暂存当前申请的用户信息
+    char        key[10], key1[10];
+    struct log
+    {
+        char    flag;
+        char    name[10];
+        char    passwd[10];
+    }apply;
+
+    memset(&apply, 0, sizeof(apply));
+    printf("请输入用户名：");
+    cls();                                      //清除输入缓存
+    scanf("%s", apply.name);
+    while(1)
+    {
+        printf("请输入密码：");
+        inputkey(key);
+        printf("请再次输入密码：");
+        inputkey(key1);
+        if(strcmp(key, key1) != 0)
+        {
+            printf("两次输入密码不相同，按任意键返回重新输入！\n");
+            getchar();
+        }
+        else 
+        {
+            strcpy(apply.passwd, key);
+            break;
+        }
+    }
+
+    apply.flag = 'a';
+    memcpy(register_buf, &apply, sizeof(apply));
+    send(conn_fd, register_buf, 1024, 0);
+
+    memset(register_buf, 0, sizeof(register_buf));
+    recv(conn_fd, register_buf, 1024, 0);
+    if(strcmp(register_buf, "n") == 0)
+    {
+        printf("\n 用户名已被使用!\n");
+        getchar();
+        return (0);
+    }
+    memcpy(&user_temp, register_buf, sizeof(register_buf));
+    printf("user:%s\n", user_temp.username);
+    strcpy(password, apply.passwd);
+    user = user_temp;
+
+    return 1;
 }
--- ChatRoom/server.c
+++ ChatRoom/server.c
@@ -36,7 +36,7 @@ struct group                                        //群组结构体
     char        group_name[10];                     //群名称
     char        admin_name[10];                     //群管理员
     int         member_num;                         //群成员数量
-    struct info member[20];                         //群成员
+    struct info member[10];                         //群成员
 };
 
 struct users 
@@ -45,25 +45,38 @@ struct users
     int         friends_num;                        //好友数
     int         group_num;                          //群组数
     struct info user;                               //用户个人信息
-    struct info friend[20];                         //好友信息
+    struct info friend[10];                         //好友信息
     struct group group[5];                          //群组
     struct users * next;
 };
 
+struct chat
+{
+    int         mark;                               //消息标志
+    char        from[10];                           //消息来自
+    char        to[10];                             //消息发往
+    char        ask[10];
+    char        time[30];                           //时间
+    char        news[500];                          //聊天信息
+};
+
+struct log
+{
+    char        flag;                               //登陆和注册的标志
+    char        name[10];
+    char        pwd[10];
+};
+
 void my_err(const char *, int);                     //错误处理函数
 void quit(void *);                                  //服务器退出函数
-void * client (void *);                              //单独的处理客户端处理线程
+void * client (void *);                             //单独的处理客户端处理线程
+void save();                                        //用户数据保存
+struct users * readuser();                          //用户数据读取
+struct users * apply(struct log, int);              //申请账号
 
 pthread_mutex_t         mutex;                      //锁
 struct users * head = NULL;                         //用户数据链表的头指针
 struct users * current_user;                        //指向当前用户的指针
-
-void my_err(const char * err_string, int line)      //自定义出错处理函数
-{
-    fprintf(stderr, "line: %d", line);
-    perror(err_string);
-    exit(1);
-}
 
 int main(void)
 {
@@ -78,12 +91,14 @@ int main(void)
     if(sock_fd < 0)
     {
         my_err("socket", __LINE__);
+        exit(1);
     }
 
     optval = 1;                                     //设置该套接字使之可以重新绑定端口，optval中存有待设定的值
     if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, (void *)&optval, sizeof(int)) < 0)
     {
         my_err("setsockopt", __LINE__);
+        exit(1);
     }
 
     memset(&serv_addr, 0, sizeof(struct sockaddr_in));      //初始化服务器端口地址
@@ -94,11 +109,13 @@ int main(void)
     if(bind(sock_fd, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr_in)) < 0)    //将套接字绑定到本地端口
     {
         my_err("bind", __LINE__);
+        exit(1);
     }
 
     if(listen(sock_fd, LISTENQ) < 0)                //将套接字转化为监听字
     {
         my_err("listen", __LINE__);
+        exit(1);
     }
     
     for(i = 0; i < LISTENQ; i++ )                   //初始化连接套接字
@@ -121,6 +138,7 @@ int main(void)
         if(conn_fd < 0)                                                             //出错处理
         {
             my_err("accept", __LINE__); 
+            exit(1);
         }
         
         conn[i].fd = conn_fd;                                   //若该连接套接字可用，将其使用赋值给连接套接字队列中未被使用的一个
@@ -133,7 +151,70 @@ int main(void)
 
 void * client (void * arg)
 {
+    int             i = *(int *)arg;
+    char            flag[1024] ;
+    char            recv_buf[1024];
+    struct users*   p_user;
+    int             ret;
 
+    while(1)
+    {
+        struct log  log;
+        memset(flag, 0, sizeof(flag));
+
+        if((ret = recv(conn[i].fd, flag, 1024, 0)) < 0)
+        {
+            my_err("apply", __LINE__);
+            pthread_exit(0);
+        }
+        
+        pthread_mutex_lock(&mutex);         //加锁
+        memcpy(&log, flag, 1024);
+        
+        head = readuser();
+
+        if(log.flag == 'a')                 //注册请求
+        {
+            p_user = apply(log, i);
+            pthread_mutex_unlock(&mutex);   //解锁
+            if(p_user != NULL)
+            {
+                break;
+            }
+        }
+
+        if(log.flag == 'b')                 //登陆请求
+        {
+            p_user = login(log, i);
+            pthread_mutex_unlock(&mutex);
+            if(p_user != NULL)
+            {
+                break;
+            }
+        }
+
+        if(log.flag == 'q')
+        {
+            pthread_mutex_unlock(&mutex);
+            ret = send(conn[i].fd, "y", 1024, 0);
+            close(conn[i].fd);
+            conn[i].fd = -1;
+            pthread_exit(0);
+        }
+    }
+
+    while(1)
+    {
+        struct chat         chat;
+        memset(&chat, 0, sizeof(chat));
+        memset(recv_buf, 0, sizeof(recv_buf));
+        ret = recv(conn[i].fd, recv_buf, 1024, 0);
+        memcpy(&chat, recv_buf, sizeof(recv_buf));
+        switch(chat.mark)
+        {
+            
+        }
+    }
 }
 
 void quit(void *arg)                        //服务器退出函数
@@ -158,4 +239,67 @@ void quit(void *arg)                        //服务器退出函数
             exit(1);
         }
     }
+}
+
+
+void save()                                             //用户数据保存
+{
+    FILE *          fp;
+    struct users*   p = head->next;
+    if((fp = fopen("usersdata.db", "wb")) == NULL)
+    {
+        printf("Cann't open usersdata.db\n");
+        exit(0);
+    }
+    while(p != NULL)
+    {
+        fwrite(p, sizeof(struct users), 1, fp);
+        p = p->next;
+    }
+    fclose(fp);
+}
+
+struct users * readuser()
+{
+    FILE *          fp;
+    struct users    *p1, *p2;
+    
+    if((fopen("usersdata.db", "rb")) == NULL)               //先判断文件是否存在，不存在则新建
+    {
+        fp = fopen("usersdata.db", "wb");                   //不存在，新建
+        fclose(fp);
+    }
+    
+    if((fp = fopen("usersdata.db", "rb")) == NULL)
+    {
+        printf("Cann't open usersdata.db\n");
+        exit(0);
+    }
+    head = p1 = (struct users*)malloc(sizeof(struct users));
+    p2 = (struct users *)malloc(sizeof(struct users));
+    fread(p2, sizeof(struct users), 1, fp);
+    while(!feof(fp))
+    {
+        p1->next = p2;
+        p1 = p2;
+        p2 = (struct users *)malloc(sizeof(struct users));
+        fread(p2, sizeof(struct users), 1, fp);
+    }
+    p1->next = NULL;
+    free(p2);
+    fclose(fp);
+    return head;
+}
+
+void my_err(const char * err_string, int line)      //自定义出错处理函数
+{
+    fprintf(stderr, "line: %d", line);
+    perror(err_string);
+}
+
+struct users * apply(struct log log, int i)
+{
+    char            enroll_buf[1024];
+    struct log      log;
+    struct users
 }
